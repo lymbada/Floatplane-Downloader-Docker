a8fde650331887232f7a505196cca1bba3d92074
Matt Tilney <matt.tilney@tasktop.com>
2019-07-01 14:12:10 +1000

    RunScript now moves more files to ./config
    The updated now allows for a list of configured files to be moved (and then sim-linked) to the ./config directory, which should be a volume on your system

--- run.sh
+++ run.sh
@@ -1,14 +1,30 @@
 #!/bin/bash
 echo "Running run.sh startup script"
-if [ ! -f /Floatplane-Downloader/config/settings.json ]; then
-	echo "No file, now copying settings file"
-	cp /Floatplane-Downloader/settings.json /Floatplane-Downloader/config/settings.json
+declare -a configFiles=("broken.json" "float.log" "videos.json.backup" "videos.json" "partial.json" "settings.json")
+configDirectory="/Floatplane-Downloader/config"
+
+# Setup the config directory, if not already created. This allows for out-of-docker testing
+if [ ! -d $configDirectory ]; then
+	mkdir -p $configDirectory
 fi
 
-if [ -f /Floatplane-Downloader/settings.json ]; then
-	rm /Floatplane-Downloader/settings.json
-fi
+arraylength=${#configFiles[@]}
+# check if each of the above files exist in the correct location, if not link or copy them over
+for (( i=1; i<${arraylength}+1; i++ ));
+do
+	if [ ! -f $configDirectory/${configFiles[$i-1]} ]; then
+		# currently the settings files do not exist, they need to be copied over
+		if [ ! -f "/Floatplane-Downloader/${configFiles[$i-1]}" ]; then
+			echo "Missing critical file '/Floatplane-Downloader/${configFiles[$i-1]}', you'l need this to run"
 
-ln -s /Floatplane-Downloader/config/settings.json /Floatplane-Downloader/settings.json
+		else
 
- node float.js
+			# file exists, but not in the config directory; then link it back
+			mv "/Floatplane-Downloader/${configFiles[$i-1]}" $configDirectory/${configFiles[$i-1]}
+			ln -s $configDirectory/${configFiles[$i-1]} "/Floatplane-Downloader/${configFiles[$i-1]}"
+		fi
+	fi
+
+  #echo $i " / " ${arraylength} " : " ${array[$i-1]}
+done
+node float.js
